1ed122cfde912873ef4891463bcd1f43de525c25
Edoardo Tubia <tubia.edoardo@gmail.com>
2026-07-19 18:25:49 +0200

    sync: aggiorna utenti da Gestione Utenti

--- app2.js
+++ app2.js
@@ -53,7 +53,8 @@ const DEFAULT_USERS = {
     '03 jean-pierre': { password: 'jean-pierre', role: 'USER' },
     '04 stefano': { password: 'stefano', role: 'USER' },
     '05 marco': { password: 'marco', role: 'USER' },
-    '06 susik': { password: 'susik', role: 'USER' }
+    '06 susik': { password: 'susik', role: 'USER' },
+    '07 giorgio': { password: 'giorgio', role: 'USER' }
 };
 
 const GITHUB_REPO = 'Edoardo1953/DASHBOARD_BRASIL';
